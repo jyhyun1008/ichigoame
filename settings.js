fccc5c146d4c80f0e2a3e41f5901c254a3f4e6b0
jyhyun1008 <hyun1008@kasi.re.kr>
2024-03-03 23:01:24 +0900

    아이콘 변경, 메인색 변경

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const SITE_DESCRIPTION = "코바늘 구체관절인형을 만들어요"
 const SITE_FOOTER_HTML = "Copyright 2024. 이치고아메(Ichigoame). All rights reserved."
 
 // 테마
-const ACCENT_COLOR = "#fc5a8d"
+const ACCENT_COLOR = "#ff2e67"
 const BG_COLOR = "#eee"
 
 // 깃허브 정보
